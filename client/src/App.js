e0f43cf55885f74209b29fa78f07ff1b60fdc64d
Rishabh Mishra <55771529+rishabhborn2win@users.noreply.github.com>
2020-10-17 14:05:33 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,6 @@ import Display from './components/best_services/component.bestServices';
 function App() {
     return (
         <div>
-            Hello client
             {/* Our best services carousel (Kunal Prakash) */}
             <Display />
         </div>
